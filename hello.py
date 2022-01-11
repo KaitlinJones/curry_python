f28e0c43edbbcc85d90dee7d946cbb7c3bd2f96a
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-11 14:42:34 -0800

    indexing

--- hello.py
+++ hello.py
@@ -1,10 +1,4 @@
-# 1:22:40 STRINGS
-# concatenation 
-poem = """This is all combined \
-as one happy string
-what was that sound?
-it was a doorbell, ring.
-When I see you, my heart sing
-here plz take this diamond ring
-"""
-print(poem)
+
+# indexing
+poem = "Where am I?"
+print(poem[50])
