014eca33092ac51ffe1975cb5b04353f081cd13c
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-11 13:47:06 -0800

    1:12:19

---
+++ hello.py
@@ -0,0 +1,23 @@
+"""
+import math 
+
+result = 10 // 3 
+
+print(result)
+"""
+
+# / float division
+# // int division, math.floor
+
+"""
+pizza = 10
+people = 3
+
+print("leftover", pizza % people) 
+
+"""
+
+number = 34545
+limit = 10
+
+print(number % limit)
