9ce945a5710dcc0a2b5f71dda412d2d1997ba968
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-11 15:27:36 -0800

    len function

--- hello.py
+++ hello.py
@@ -1,13 +1,5 @@
 
-# id function
+msg = "please subscribe"
+#print(msg[15])
 
-task = ["Subscribe"]
-different = task
-
-different[0] = 'hey' 
-
-print(task)
-
-
-# mutable and immutable objects 
-# strings do not support item assignment
+print(len(msg))
