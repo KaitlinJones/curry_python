bbbf5c25259b44bcd8602726331fc1f211976d36
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 01:48:13 -0800

    lists review

--- hello.py
+++ hello.py
@@ -1,13 +1,8 @@
 # lists
+my_favorite_things = ["working out", 7, ['amazon prime','netflix']]
+least_favorite_things = ["onions", "javascript"]
 
-import copy
-
-my_favorite_things = ["working out", 7, ['amazon prime', ['netflix', 'hulu']]]
-c = copy.deepcopy = my_favorite_things.copy()
-
-print(c)
-
-print(id(c[2]))
-print(id(my_favorite_things[2]))
-
-print(my_favorite_things, c)
+least_favorite_things = least_favorite_things + ["editing"]
+print(least_favorite_things)
+least_favorite_things.append("editing")
+print(least_favorite_things)
