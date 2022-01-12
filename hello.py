1ed07acafb6428093fdbf34365189204bd4487dd
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 00:59:49 -0800

    parens, 2:13:21

--- hello.py
+++ hello.py
@@ -1,4 +1,18 @@
+import math
+age = 15
+age_str = str(age)
+id_age_str = id(age_str)
+other = math.floor(2.6)
+added = id_age_str + other
+added_str = str(added)
+length = len(added_str)
 
-msg = "please subscribe"
+print(length)
+print(added_str)
+print(added)
+print(other)
+print(id_age_str)
 
-print("me")
+
+print(len(str(id(str(age))+ math.floor(2.6))))
+
