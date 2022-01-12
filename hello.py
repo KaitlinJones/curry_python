2524cb754938363faee07413c01e1159026985b1
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 12:17:40 -0800

    elifs statements 03:20:41

--- hello.py
+++ hello.py
@@ -2,9 +2,16 @@
 print("what is your age?")
 age = int(input())
 
-if age > 20:
+if age > 65:
+    print("Hey, you get a special discount!")
+elif age == 21:
+    print("SUPER SPECIAL!")
+elif age > 20:
     print("Welcome to our app!")
-
+else:
+     print("You are not old enough")
+    
+print("Thanks for trying our app!")
 
 
 
