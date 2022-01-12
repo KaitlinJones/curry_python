b73579caa51d783b3d2f76eb44cce6d04ada50d0
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 01:22:58 -0800

    list mutability

--- hello.py
+++ hello.py
@@ -4,7 +4,9 @@ ages = [12,18,28]
 people = ["Caleb", "Sabrina", "emily"]
 
 my_favorite_things = ["working out", 7, ['amazon prime', 'netflix']]
-
-print(ages)
-print(people)
+print(id(my_favorite_things))
 print(my_favorite_things)
+my_favorite_things[0] = "Walking"
+print(id(my_favorite_things))
+print(my_favorite_things)
+print(len(my_favorite_things))
