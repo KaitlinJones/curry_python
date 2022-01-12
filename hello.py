366a3ffb3e10e1eff618f7ced203f5b9c50f26c4
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 12:01:18 -0800

    beginning logic

--- hello.py
+++ hello.py
@@ -1,15 +1,5 @@
-# input
-print(type(float("5")))
-
-print("Hey, what's your name?")
-name = input()
-print("Hello " + name)
-
-print("What's your favorite number?")
-fav_num = int(input())
-print("Give us another number: ")       
-fav_num2 = int(input())
-
-
-print(str(fav_num) + " + " + str(fav_num2) + " = " + str(fav_num + fav_num2))
+# logic
+age = 15
+can_drink = age > 20
+print(can_drink)
 
