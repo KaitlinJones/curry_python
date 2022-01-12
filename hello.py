97449a88ff38fb47f1fb819e38a29a1c995c7e04
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 01:33:29 -0800

    shallow copy lists

--- hello.py
+++ hello.py
@@ -1,12 +1,10 @@
 # lists
-ages = [12,18,28]
 
-people = ["Caleb", "Sabrina", "emily"]
+my_favorite_things = ["working out", 7, ['amazon prime', ['netflix', 'hulu']]]
+copy = my_favorite_things.copy()
 
-my_favorite_things = ["working out", 7, ['amazon prime', 'netflix']]
-print(id(my_favorite_things))
-print(my_favorite_things)
-my_favorite_things[0] = "Walking"
-print(id(my_favorite_things))
-print(my_favorite_things)
-print(len(my_favorite_things))
+print(copy)
+
+copy[0]= "walking"
+
+print(my_favorite_things[2][1][0])
