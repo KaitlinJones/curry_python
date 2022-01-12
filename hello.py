e668f52589eae0a14ea1b85e9c801c0b279ad797
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 02:22:49 -0800

    input/type conversion 3:00:49

--- hello.py
+++ hello.py
@@ -1,10 +1,15 @@
 # input
+print(type(float("5")))
+
 print("Hey, what's your name?")
 name = input()
 print("Hello " + name)
 
 print("What's your favorite number?")
-fav_num = input()
+fav_num = int(input())
 print("Give us another number: ")
-fav_num2 = input()
-print(fav_num + fav_num2)
+fav_num2 = int(input())
+
+
+print(str(fav_num) + "+" + str(fav_num2) + "=" + str(fav_num + fav_num2))
+
