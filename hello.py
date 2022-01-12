f54ab4b90f1348b27d6a64905b839ceae84c702f
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 02:03:43 -0800

    more input

--- hello.py
+++ hello.py
@@ -1,5 +1,10 @@
 # input
 print("Hey, what's your name?")
 name = input()
+print("Hello " + name)
 
-print(name)
+print("What's your favorite number?")
+fav_num = input()
+print("Give us another number: ")
+fav_num2 = input()
+print(fav_num + fav_num2)
