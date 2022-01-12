472bace50cbb8436af0e9e17f7c35acdff42fff6
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 11:33:44 -0800

    User input review

--- hello.py
+++ hello.py
@@ -7,9 +7,9 @@ print("Hello " + name)
 
 print("What's your favorite number?")
 fav_num = int(input())
-print("Give us another number: ")
+print("Give us another number: ")       
 fav_num2 = int(input())
 
 
-print(str(fav_num) + "+" + str(fav_num2) + "=" + str(fav_num + fav_num2))
+print(str(fav_num) + " + " + str(fav_num2) + " = " + str(fav_num + fav_num2))
 
