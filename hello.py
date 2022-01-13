bbaa39239c0dca55af35a345af18810ee3d60a74
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-13 01:08:44 -0800

    while/for comparison

--- hello.py
+++ hello.py
@@ -3,10 +3,11 @@
 i = 0 # intialization 
 while i < 10 : # condition
     print(i)
-    i += 1
-# counts 1-10
+    i += 1 # update
+# counts up to 10
+print()
 
-i = 10
-while i >= 0:
-    print (i)
-    i -= 1
+for i in range(0, 10, 1):
+    print(i)
+
+# same out put; while loop is more versatile, for loop is less verbose 
