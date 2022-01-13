c9222dc4deb72bda5cc1f78d5f5f92ae30216407
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-13 01:34:29 -0800

    flags 4:24:13

--- hello.py
+++ hello.py
@@ -1,16 +1,17 @@
-# else clause combined with while loop
-numbers = [5, 3, 6, 7, 4, 2, 4, 3]
+# flags
+numbers = [5, 3, 6, 70, 4, 2, 4, 3]
 
 i = 0
 square = 500
+success = False #flag
 
 while i < len(numbers):
     if numbers[i] ** 2 > square:
         print (numbers[i], "squared is larger than", square)
+        success = True #flag
         break
-    else:
-        print(numbers[i], "squared is not larger than", square)
+    print(numbers[i], "squared is not larger than", square)
     i += 1
 
-else: 
-    print("None squared are larger than", square)
+if not success: #flag test
+    print("None of them was large enough")
