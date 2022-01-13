ae39f9a78e1532993ddd98a8c8ebde68a620239e
Kaitlin Jones <kaitlinjones@joneskaitlin.com>
2022-01-12 21:29:55 -0800

    intro to range

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
-# for loop
-for i in range(231, 501):
+# control the count 
+for i in range(230, 501, 2):
+    print(i, i + 1, end = " ")
+
+print()
+print()
+
+# backwards 
+for i in range(200, 0, -1):
     print(i, i + 1, end = " ")
 
 print()
